38e03ce0e1de6e6614d4d5b661859de30441268d
Samuel <mudocontato@gmail.com>
2023-11-05 04:44:38 -0300

    update

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 5;
+let numeroLimite = 10;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
+console.log(gerarNumeroAleatorio())
 
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
@@ -11,7 +12,7 @@ function exibirTextoNaTela(tag, texto) {
 
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 5');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
 }
 
 exibirMensagemInicial();
